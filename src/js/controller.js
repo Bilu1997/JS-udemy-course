e5f1c21045990fcf089e13630e8eb6403fa0a9bc
Bilu1997 <39301208+Bilu1997@users.noreply.github.com>
2021-03-04 15:31:31 +0100

    Added rendering results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,14 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
-import SearchView from './views/searchView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime';
-import searchView from './views/searchView.js';
 
-const recipeContainer = document.querySelector('.recipe');
+if (module.hot) {
+  module.hot.accept();
+}
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -32,12 +34,16 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
     // 1 Get search query
     const query = searchView.getQuery();
     if (!query) return;
 
     // 2 Load search results
     await model.loadSearchResults(query);
+
+    //render results
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.error(err);
   }
